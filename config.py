deda6756f88ae007b4774273b27bfa2ca23d22e0
sw_cheats 1 <suwa261101@gmail.com>
2025-05-08 14:15:11 +0200

    config.py added

---
+++ config.py
@@ -0,0 +1,16 @@
+LLM_CONFIG = {
+    "config_list": [
+        {
+            "model": "open-mistral-nemo",
+            "api_key": "[YOUR_API_KEY]",
+            "api_type": "mistral",
+            "api_rate_limit": 0.25,
+            "repeat_penalty": 1.1,
+            "temperature": 0.0,
+            "seed": 42,
+            "stream": False,
+            "native_tool_calls": False,
+            "cache_seed": None,
+        }
+    ]
+}
